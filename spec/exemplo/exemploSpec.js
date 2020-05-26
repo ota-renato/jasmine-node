dbcb1d54f3bf14aa1804c0bda7c13e5f38c23f29
ota-renato <30956931+ota-renato@users.noreply.github.com>
2020-05-25 22:04:11 -0300

    Arquivos do projeto local

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,10 @@
+describe("Suite de teste de exemplo com NodeJS", () => {
+
+    it("Deve demonstrar o uso do Jasmine com o NodeJS", () => {
+        expect(true).toEqual(true);
+    });
+
+    it("Deve demonstrar o uso do Karma em mode de escuta", () => {
+        expect(1).toEqual(1);
+    });
+});
